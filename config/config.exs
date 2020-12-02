cf4bf3aeabec67add48c284d83ec815ecd75673c
David Chen <darkbaby123@gmail.com>
2020-12-02 16:25:26 +0800

    feat: use Config instead of Mix.Config

--- config/config.exs
+++ config/config.exs
@@ -1,13 +1,13 @@
 # This file is responsible for configuring your umbrella
 # and **all applications** and their dependencies with the
-# help of Mix.Config.
+# help of Config.
 #
 # Note that all applications in your umbrella share the
 # same configuration and dependencies, which is why they
 # all use the same configuration file. If you want different
 # configurations or dependencies per app, it is best to
 # move said applications out of the umbrella.
-use Mix.Config
+import Config
 
 # Configure Mix tasks and generators
 config :demo,
@@ -40,4 +40,4 @@ config :phoenix, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env()}.exs"
+import_config "#{config_env()}.exs"
